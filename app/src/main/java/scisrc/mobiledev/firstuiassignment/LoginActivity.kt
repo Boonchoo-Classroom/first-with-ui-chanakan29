29e4b709aded8b561a38113157622e4da7970372
Test01 <Chanakan.bunk@ku.th>
2025-01-18 22:41:21 +0700

    Finish

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
@@ -1,7 +1,13 @@
 package scisrc.mobiledev.firstuiassignment
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.graphics.Typeface
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var backToMainBtn: Button
+    private lateinit var backToMainBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +26,29 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         backToMainBtn = findViewById(R.id.loginBackToMainBtn)
 
-        backToMainBtn.setOnClickListener() {
+        backToMainBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
             finish()
         }
+
+        val signUpTextView = findViewById<TextView>(R.id.signUpTextView)
+
+        val text = "Don't have an account? Sign Up"
+
+        val spannable = SpannableString(text)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        spannable.setSpan(boldSpan, text.indexOf("Sign Up"), text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        signUpTextView.text = spannable
+
+        signUpTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/RegisterActivity.kt
@@ -1,7 +1,13 @@
 package scisrc.mobiledev.firstuiassignment
 
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.Button
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StyleSpan
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var backToMainPageBtn: Button
+    private lateinit var backToMainBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +27,28 @@ class RegisterActivity : AppCompatActivity() {
             insets
         }
 
-        backToMainPageBtn = findViewById(R.id.registBackToMainBtn)
+        backToMainBtn = findViewById(R.id.registBackToMainBtn)
 
-        backToMainPageBtn.setOnClickListener() {
+        backToMainBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
             finish()
+        }
+
+        val loginTextView = findViewById<TextView>(R.id.loginTextView)
+
+        val text = "Already have an account? Login"
+
+        val spannable = SpannableString(text)
+        val boldSpan = StyleSpan(Typeface.BOLD)
+        spannable.setSpan(boldSpan, text.indexOf("Login"), text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        loginTextView.text = spannable
+
+        loginTextView.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
